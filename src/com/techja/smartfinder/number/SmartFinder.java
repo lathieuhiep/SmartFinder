613419b5e732c2fe48c852d2c7fc5d10e0ccbaa7
lathieuhiep <khacdiepkma90@gmail.com>
2024-05-08 01:03:46 +0700

    edit

--- src/com/techja/smartfinder/number/SmartFinder.java
+++ src/com/techja/smartfinder/number/SmartFinder.java
@@ -7,12 +7,46 @@ public class SmartFinder {
         this.soN = soN;
     }
 
+    // Tim UCLN
+    private int timUCLN(int a, int b) {
+        if (a < b) {
+            int temp = a;
+
+            a = b;
+            b = temp;
+        }
+
+        while (b != 0 ) {
+            int temp = b;
+
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    // tim UC 2 so
+    private String timUC(int a, int b) {
+        int UCLN = timUCLN(a, b);
+        String dsUC = "";
+
+        for (int i = 0; i <= Math.sqrt(UCLN) ; i++) {
+            if (UCLN % i == 0) {}
+        }
+        
+        return dsUC;
+    }
+
+
     public void findLargeArea() {
         // in ra cac cap a va b sao cho a + b < 50
         int a = 1;
 
         while (a <= soN) {
             for (int b = a + 1; b < soN; b++) {
+                String UC = timUC(a, b);
+
                 if (a + b < 50) {
                     System.out.println("(" + a + "," + b + ") ");
                 }
